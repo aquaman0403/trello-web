1327e60c981bde59ac27e1eda9d4a1c0c6bd75cd
Vladimir Duck <vanducpham2k3nh@gmail.com>
2025-04-18 22:43:16 +0700

    base ui

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import CssBaseline from '@mui/material/CssBaseline';
 import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles';
-import { ThemeProvider } from '@mui/material/styles';
 import App from './App.jsx'
 import theme from './theme.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
+    <CssVarsProvider theme={theme}>
       <CssBaseline/>
       <App />
-    </ThemeProvider>
+    </CssVarsProvider>
   </StrictMode>,
 )
--- src/theme.jsx
+++ src/theme.jsx
@@ -1,8 +1,13 @@
 import { createTheme } from '@mui/material/styles'
-import { cyan, deepOrange, orange, red, teal } from '@mui/material/colors';
+import { cyan, deepOrange, orange, teal } from '@mui/material/colors';
 
 // Create a theme instance.
 const theme = createTheme({
+  trello: {
+    appBarHeight: '48px',
+    boardBarHeight: '58px',
+  },
+
   colorSchemes:{
     light:{
         palette: {
